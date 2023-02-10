2e466b84976957fd8739fb24bfb4a26a3e919c82
Yuriy Dupyn <2153100+omedusyo@users.noreply.github.com>
2023-02-10 21:45:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Building a file-system simulator in PureScript
---
+++ index.js
@@ -0,0 +1,10 @@
+import * as Main from './output/Main/index';
+
+function main() {
+  Main.main();
+}
+
+
+main();
+
+console.log("Starting app");
